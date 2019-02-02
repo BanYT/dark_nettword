4aace7897d817318eb75b97d1ec77fdbea28625c
BanYT <44928120+BanYT@users.noreply.github.com>
2019-02-02 20:32:40 +0100

    Update ann.js

--- commands/ann.js
+++ commands/ann.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
   if (message.author.id === "533765048171102228" || "409351580551479308") {
 } else {
-message.channel.send("**Only Eliffeur can do that ! So Don't Try Again.**").then(x=>x.delete(3000))
+message.channel.send("**Only Zloy can do that ! So Don't Try Again.**").then(x=>x.delete(3000))
 }
 message.guild.members.map(x=>x.sendMessage(args.join(" ")))
 
