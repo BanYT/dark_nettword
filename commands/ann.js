a503cbada1a7372277ed628b9ee9085166f6ae63
BanYT <44928120+BanYT@users.noreply.github.com>
2019-02-02 20:26:29 +0100

    Update ann.js

--- commands/ann.js
+++ commands/ann.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-  if (message.author.id === "533303649011499019") {
+  if (message.author.id === "409351580551479308" || "533303649011499019" || "539434441097871361") {
 } else {
 message.channel.send("**Only Eliffeur can do that ! So Don't Try Again.**").then(x=>x.delete(3000))
 }
