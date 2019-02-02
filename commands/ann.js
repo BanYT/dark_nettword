79c65eba19527d261ced73f8e560e5892b1c5f69
BanYT <44928120+BanYT@users.noreply.github.com>
2019-02-02 15:47:32 +0100

    Update ann.js

--- commands/ann.js
+++ commands/ann.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
   if (message.author.id === "409351580551479308") {
 } else {
 message.channel.send("**Only Ban can do that ! So Don't Try Again.**").then(x=>x.delete(3000))
-
+}
 message.guild.members.map(x=>x.sendMessage(args.join(" ")))
 
   }
