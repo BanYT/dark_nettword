0cd5492b34212e849e5b23969443d1befc265a75
BanYT <44928120+BanYT@users.noreply.github.com>
2019-02-02 15:34:18 +0100

    Create cmdloader.js

---
+++ util/cmdloader.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const { promisify } = require("util");
+const readdir = promisify(require("fs").readdir);
+
+module.exports = async (client) => {
+    client.commands = new Discord.Collection();
+    client.aliases = new Discord.Collection();
+    client.miscCommands = new Discord.Collection();
+
+const miscFiles = await readdir("./commands/");
+
+
+miscFiles.forEach(f => {
+    if (!f.endsWith(".js")) return;
+		let misc = require(`../commands/${f}`);
+    client.miscCommands.set(misc.help.name, misc);
+    client.commands.set(misc.help.name, misc);
+	client.aliases.set(misc.help.alias, misc);
+	});
+console.log(`loaded ${client.commands.size} commands`);
+console.log(`loaded ${client.aliases.size} aliases`);
+}
